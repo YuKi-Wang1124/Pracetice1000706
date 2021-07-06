04cdd680534142cf443ac6384dbf8e18bd786083
yuki <wkiki1124@gmail.com>
2021-07-06 19:36:43 +0800

    Initial Commit

---
+++ Pracetice1000706/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pracetice1000706
+//
+//  Created by 王昱淇 on 2021/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
